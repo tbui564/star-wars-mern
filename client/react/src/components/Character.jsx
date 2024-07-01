9f56c692162849fa2e2b7f87e5b047300094aa4a
Thien-Bao Bui <tbui2.trav.github@gmail.com>
2024-07-01 16:27:09 -0400

    Characters display

--- client/react/src/components/Character.jsx
+++ client/react/src/components/Character.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import CharacterCard from "./CharacterCard";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+const fetchCharacters = async (data, setData) => {
+  const url = "http://localhost:3000/api/characters";
+  try {
+    console.log("Fetching characters from URL");
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(Array.from(json));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 const Character = () => {
-    const [data, setData] = useState({})
-        const url = http://localhost:3000/api/characters";
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          const json = await response.json();
-          setData(json);
-        } catch (error) {
-          console.error(error.message);
-        }
-    
-        return ( 
-        
-            data.forEach(character => {
-                <CharacterCard character = {character} />
-        
-           })
-           
-        )
-     
-        }
-    
-      export default Character;
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchCharacters(data, setData);
+  }, []);
+ 
+  console.log(data[0]?.id)  ;
+
+  return (
+    <>
+    {data.map(character => <CharacterCard character={character} />)}
+    </>
+  )
+}
+
+export default Character;
--- client/react/src/components/CharacterCard.jsx
+++ client/react/src/components/CharacterCard.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-const CharacterCard = ({Character}) => {
+const CharacterCard = ({ character }) => {
     return (
-   <div className ="character-card">
-    <h2>{Character.name}</h2>
-    <ul>
-        {Character.map(attribute => {
-            (
-                
-                <li key={attribute}>{attribute}: {Character[attribute]}
-
-                </li>
-            )
-        
-        })} 
-    </ul>
-
-   </div>
-)}
+        <div className="character-card">
+            <h2>{character?.name}</h2>
+            <ul>
+                <li>ID: {character?.id}</li>
+                <li>Name: {character?.name}</li>
+                <li>Gender: {character?.gender}</li>
+                <li>Skin Color: {character?.skin_color}</li>
+                <li>Hair Color: {character?.hair_color}</li>
+                <li>Height: {character?.height}</li>
+                <li>Eye Color: {character?.eye_color}</li>
+                <li>Mass: {character?.mass}</li>
+                <li>Homeworld ID: {character?.homeworld}</li>
+                <li>Birth: {character?.birth_year}</li>
+            </ul>
+        </div>
+    )
+}
 
 export default CharacterCard;
